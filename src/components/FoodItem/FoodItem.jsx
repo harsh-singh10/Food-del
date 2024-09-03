c2bd2e2da4573c80a7ee22d2091dee89e897b2ab
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-09-03 23:49:43 +0530

    create food display and food item components

---
+++ src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,29 @@
+import React, { useContext, useState } from 'react'
+import './FoodItem.css'
+import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext';
+
+
+const FoodItem = ({ image, name, price, desc , id }) => {
+
+    const [itemCount, setItemCount] = useState(0);
+    // const {cartItems,addToCart,removeFromCart,url,currency} = useContext(StoreContext);
+
+    return (
+        <div className='food-item'>
+            <div className='food-item-img-container'>
+                <img className='food-item-image' src={image} alt="" />
+               
+            </div>
+            <div className="food-item-info">
+                <div className="food-item-name-rating">
+                    <p>{name}</p> <img src={assets.rating_starts} alt="" />
+                </div>
+                <p className="food-item-desc">{desc}</p>
+                <p className="food-item-price">${price}</p>
+            </div>
+        </div>
+    )
+}
+
+export default FoodItem
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import "./Home.css"
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 
 
@@ -14,6 +15,7 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }
