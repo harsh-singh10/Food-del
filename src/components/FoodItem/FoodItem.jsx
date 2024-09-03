df1588d44e648eced6ef8c393913dc7808785bce
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-09-04 00:02:25 +0530

    fix counter button bug

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -13,7 +13,16 @@ const FoodItem = ({ image, name, price, desc , id }) => {
         <div className='food-item'>
             <div className='food-item-img-container'>
                 <img className='food-item-image' src={image} alt="" />
-               
+               {!itemCount
+                ? <img className='add' onClick={()=>setItemCount(prev => prev+1)}  src={assets.add_icon_white} alt="" srcset="" />
+                : <div className="food-item-counter">
+                     
+                        <img src={assets.add_icon_green} onClick={()=>setItemCount(prev => prev +1)} alt="" />
+                        <p>{itemCount}</p>
+                        <img src={assets.remove_icon_red} onClick={()=> setItemCount(prev => prev-1)} alt="" />
+                       
+                     </div>
+               }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
